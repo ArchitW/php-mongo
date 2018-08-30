bb197a2b967a5c0dc4d9f4b2cb07a647609d0d77
Unknown <archit2x3@hotmail.com>
2018-08-30 17:33:12 +0530

    notes for mongodb

--- test.php
+++ test.php
@@ -29,3 +29,39 @@ var_dump($document);
 
 //time stamp conversion from unix TT to calender format
 echo date('Y-m-d', 1535630380);
+
+
+
+/*
+ * Mongo Refresher
+ * show dbs - shows database
+ * use collection : used if available or creates 1
+ * db.createCollection("CollectionName)
+ * db.collectionName.insertOne("Test Doc":"123") # key:value
+ *  => returns insertedId
+ * db.getCollectionNames(); ##shows all table
+db.collectionName.find(); #dspaly all rows
+
+select
+db.CollectionName.find({'Key':'$gt:2'})
+->Find Documents from Collection which have key greater than 2
+$gt: Greater than
+$lt: Less Then
+$eq: Equal
+
+*** Projection ***
+ *
+ * sql : select id, name , email where name ="A"
+ * mongo : db.collectionName.find({email:"A"},{"name":1, "status":1})
+ *
+ * Return all exclude few
+ *db.collectionName.find({email:"A"},{"name":0})
+ *
+ *
+ * *** UPDATE ***
+ * db.collectionName.updateOne(
+ * {'find':'value'},
+ * {$set:{'field':'value','field1':'value1'}})
+ *
+
+* */
